24312a46f511c13ddf86c32d8bc72ac8885357a7
AnnaWolska <a_wl@wp.pl>
2023-05-19 23:02:50 +0200

    black-jack 9 tests

--- player.py
+++ player.py
@@ -6,24 +6,19 @@ class Player:
 
     def take_card(self, card:Card):
         self.cards.append(card)
-        print("self.cards",self.cards)
+        print("1 self.cards", self.cards)
 
     def calculate_points(self):
         points = 0
         for card in self.cards:
-            print(card)
+            print("2 card",card)
             if card in ["Ace","Jack","Queen","King"]:
                 points += 10
             else:
                 points += int(card.value)
-                print("card.value",card.value)
+                print("3 card.value",card.value)
         return points
 
     some_card = Card(5,"hearts")
-    print("some_card",some_card)
-    print(some_card.color, some_card.value)
-
-    take_card(some_card)
-    print("cards",cards)
-
-    print(calculate_points(some_card))
+    print("4 some_card",some_card)
+    print("5 some_card.color, some_card.value", some_card.value, some_card.color )
